661d3cf634fcfceb1b5f0247c6c55996e477032a
Home-PC <JavaKira@yandex.ru>
2023-08-14 22:47:36 +0300

    bla

--- order.js
+++ order.js
@@ -1,7 +1,10 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+document.getElementById("orderTitle").innerText = id;
+
 let tg = window.Telegram.WebApp;
 tg.expand();
 tg.BackButton.show();
 tg.BackButton.onClick(() => {
     window.history.go(-1);
-    tg.BackButton.hide();
 });
