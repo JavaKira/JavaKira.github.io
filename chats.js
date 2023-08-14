0abb454cb3699667f0e45aeba4380fd035f4bd9a
Home-PC <JavaKira@yandex.ru>
2023-08-14 22:56:08 +0300

    bla

--- chats.js
+++ chats.js
@@ -1,3 +1,25 @@
+function addChat(id, avatarUrl, title, lastMessage) {
+    let ordersList = document.getElementById("chatsList");
+    ordersList.innerHTML +=
+        `<div class="bg" onclick="openChat(id)">
+            <div class="container2">
+                <div class="el">
+                    <img class="avatarSmall" src="https://yt3.googleusercontent.com/ytc/AGIKgqNVH4fYdH6c-pMZ-sUSCRsOmAeKe7S4Xx6bvW22=s900-c-k-c0x00ffffff-no-rj">
+                </div>
+                <div class="el">
+                    <h3 class="text">` + title + `</h3>
+                    <p class="hint" >` + lastMessage + `</p>
+                </div>
+            </div>
+        </div>`;
+}
+
+function openChat(id) {
+    window.open("/chat?id=" + id, "_self")
+}
+
+addChat(1, "","Название чата 1", "elit ut aliquam purus sit")
+
 let tg = window.Telegram.WebApp;
 tg.expand();
 tg.BackButton.show();
