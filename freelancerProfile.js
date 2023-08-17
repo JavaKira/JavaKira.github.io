c3eb44e2a2fd948cc40082b4ec011a9281923ab0
JavaKira <JavaKira@yandex.ru>
2023-08-17 22:55:57 +0300

    bla

--- freelancerProfile.js
+++ freelancerProfile.js
@@ -4,3 +4,7 @@ tg.BackButton.show();
 tg.BackButton.onClick(() => {
     window.history.go(-1);
 });
+
+document.getElementById("test").onclick(() => {
+    window.open("https://t.me/TestNaProesd_bot/TestShortName", "_self")
+})
