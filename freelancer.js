1437c3f69a1b8df214f7341560d5b99552ac2705
JavaKira <JavaKira@yandex.ru>
2023-08-18 12:04:44 +0300

    bla

--- freelancer.js
+++ freelancer.js
@@ -1,6 +1,6 @@
 let tg = window.Telegram.WebApp;
 const userNameSpan = document.getElementById("userName");
-userNameSpan.innerText = tg.initDataUnsafe.user.displayName;
+userNameSpan.innerText = tg.initDataUnsafe.user.first_name;
 tg.expand();
 tg.BackButton.show();
 tg.BackButton.onClick(() => {
