119485696af82c2095d0d80ee501b3342867786e
JavaKira <JavaKira@yandex.ru>
2023-08-22 01:16:56 +0300

    bla

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require("express");
+const https = require("https");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 let routes = ["/profile", "/about", "/customer", "/customerProfile", "/form", "/freelancer", "/freelancerProfile", "/orders"]
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
 })
 
 routes.forEach(route => {
     app.get(route, (req, res) => {
-        res.sendFile(__dirname + '/public' + route + '.html');
+        res.sendFile(__dirname + "/public" + route + ".html");
     })
 })
 
-app.listen(8000, '5.253.61.170', () => {
-    console.log(`Server running at 5.253.61.170:8000`);
+https.createServer(app).listen(8000, "5.253.61.170", () => {
+    console.log("server is running at port 4000")
 });
