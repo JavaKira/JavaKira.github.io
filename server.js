4c346f71fe289c4f3e84080131455ed642c0735a
JavaKira <JavaKira@yandex.ru>
2023-08-22 00:01:36 +0300

    bla

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
+import express from 'express';
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 app.listen(8000, 'localhost', () => {
