37221b1fbb38d60121ae9bfda47db514dc776647
JavaKira <JavaKira@yandex.ru>
2023-08-22 01:02:52 +0300

    bla

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-import express from 'express';
+const express = require('express');
+
 const app = express();
 
 app.use(express.static('public'));
 
+let routes = ["/profile", "/about", "/customer", "/customerProfile", "/form", "/freelancer", "/freelancerProfile", "/orders"]
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
+})
+
+routes.forEach(route => {
+    app.get(route, (req, res) => {
+        res.sendFile(__dirname + '/public' + route + '.html');
+    })
 })
 
 app.listen(8000, 'localhost', () => {
