cabafe0fe293857f91aaba8584765f2069aa7d38
JavaKira <JavaKira@yandex.ru>
2023-08-24 17:36:46 +0300

    bla

--- db/db.js
+++ db/db.js
@@ -3,15 +3,17 @@ const postgres = require("postgres");
 const PGHOST = "ep-frosty-leaf-52840909.us-east-2.aws.neon.tech";
 const PGDATABASE = "neondb";
 const PGUSER = "JavaKira";
-const PGPASSWORD = "<% PASSWORD %>";
+const PGPASSWORD = "dxS5EMUWHf7J";
 const ENDPOINT_ID = "ep-frosty-leaf-52840909";
 
 const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
 const sql = postgres(URL, {ssl: 'require'});
 
-export function getUser(id) {
+function getUser(id) {
     return sql`SELECT *
                FROM users
                `
     //WHERE user_id = ${id}
 }
+
+module.exports.getUser = getUser;
